2a04d837e9ffcc7fb16ed2293ce89dbe95002662
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2025-02-23 18:20:10 +0000

    blue tooth now looks for a device

--- pwa/home-automation-phone-app/src/App.vue
+++ pwa/home-automation-phone-app/src/App.vue
@@ -1,9 +1,36 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import ConnectDevice from "./components/ConnectDevice.vue";
+import MiddleCard from "./components/MiddleCard.vue";
+import { ref } from "vue";
+
+const device = ref(null);
+const deviceName = ref(null);
+const deviceInfo = ref(null);
+
+const connected = ref(false);
+
+const findDevice = async () => {
+    console.log("Finding device");
+    device.value = navigator.bluetooth
+        .requestDevice({
+            filters: [{ name: "homeAutomationPico" }],
+            optionalServices: ["6E400001-B5A3-F393-E0A9-E50E24DCCA9E"],
+        })
+        .then((device) => {
+            console.log("Got device:", device.name);
+        })
+        .catch((error) => {
+            console.error("Argh! ", error);
+        });
+
+    deviceName.value = device.value.name || "No device found";
+    deviceInfo.value = "Connected to: " + deviceName.value;
+};
 </script>
 
 <template>
-    <HelloWorld msg="home-automation-phone-app" />
+    <ConnectDevice @findDevice="findDevice" />
+    <MiddleCard />
 </template>
 
 <style scoped></style>
---
+++ pwa/home-automation-phone-app/src/components/ConnectDevice.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineEmits } from "vue";
+const emit = defineEmits(["findDevice"]);
+
+const findDevice = () => {
+    emit("findDevice", findDevice);
+};
+</script>
+
+<template>
+    <div
+        class="absolute top-3 right-3 w-auto h-auto flex flex-col items-center justify-center"
+    >
+        <button @click="findDevice" class="btn btn-wide">Connect Device</button>
+    </div>
+</template>
