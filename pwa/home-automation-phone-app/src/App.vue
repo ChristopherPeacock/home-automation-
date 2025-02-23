9e5aab51f46575a03eafd30d4aebf022dd5d3c34
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2025-02-23 15:11:36 +0000

    first commit

---
+++ pwa/home-automation-phone-app/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <HelloWorld msg="home-automation-phone-app" />
+</template>
+
+<style scoped></style>
---
+++ pwa/home-automation-phone-app/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+    <h1 class="text-red-500">hey</h1>
+</template>
+
+<style scoped></style>
